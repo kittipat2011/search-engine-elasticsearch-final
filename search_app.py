628b87cd68c3a3635d5999ea4e0a925de7b295b2
s <haha.com>
2022-11-13 16:28:26 +0700

    finish

--- search_app.py
+++ search_app.py
@@ -17,11 +17,12 @@ def check_info(data):
     if(data == "null" or data == "This edition doesn't have a description yet. Can you add one?"):
         return "undefined"
     elif(data == '2x'):
-        return "./static/img/book.png"
+        return "./static/img/noimg.png"
     else:
         return data
 
 app.jinja_env.globals.update(check_info = check_info) 
+
 
 @app.route('/search')
 def search():
@@ -51,9 +52,20 @@ def search():
     }
 
     res = es.search(index='books',body=body)
+    value = res['hits']['total']['value']
+    # calcuate last page
+    last_page = 0
+    for i in range(value):
+        if(i < page_no * 10):
+            last_page+=1
+
+    # calculate first page
+    first_page = page_no - 1
+    first_page = (first_page*page_size)+1
+
     hits = [{'title': doc['_source']['title'], 'author': doc['_source']['author'], 
     'public_date': doc['_source']['public_date'],'image-src': doc['_source']['image-src'],
     'description':doc['_source']['description'],'web-scraper-start-url':doc['_source']['web-scraper-start-url'],
-    'rating':doc['_source']['rating']} for doc in res['hits']['hits']]
+    'rating':doc['_source']['rating'],'score':doc['_score']} for doc in res['hits']['hits']]
     page_total = math.ceil(res['hits']['total']['value']/page_size)
-    return render_template('search.html',keyword=keyword, hits=hits, page_no=page_no, page_total=page_total)
+    return render_template('search.html',keyword=keyword, hits=hits, page_no=page_no, page_total=page_total,value=value,last_page=last_page,first_page=first_page)
